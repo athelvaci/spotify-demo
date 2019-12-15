4ef6e343e010c211319c981a25908b44ef2d5fff
athelvaci <athelvaci@gmail.com>
2019-12-15 23:38:57 +0300

    initial commit

---
+++ src/main/java/com/app/spotifydemo/SpotifyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.app.spotifydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyDemoApplication.class, args);
+    }
+
+}
